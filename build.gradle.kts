232aec97473bb1a298a0ed59a0c7e9642b063b83
Кирилл Матвеев <playnikov@gmail.com>
2025-11-20 20:33:41 +0300

    add hikari and flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 val exposed_version: String by project
-val h2_version: String by project
+val postgres_version: String by project
 val koin_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val flyway_version: String by project
+val bcrypt_version: String by project
+val hikari_version: String by project
+val hoplite_version: String by project
 
 plugins {
     kotlin("jvm") version "2.2.20"
@@ -24,14 +28,23 @@ dependencies {
     implementation("io.ktor:ktor-server-call-logging")
     implementation("io.ktor:ktor-server-content-negotiation")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
+
+    // Database
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
-    implementation("io.insert-koin:koin-ktor:$koin_version")
-    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
-    implementation("io.ktor:ktor-server-netty")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+    implementation("org.postgresql:postgresql:$postgres_version")
+    implementation("com.zaxxer:HikariCP:$hikari_version")
+    implementation("org.flywaydb:flyway-core:$flyway_version")
+
+    // Security
+    implementation("at.favre.lib:bcrypt:$bcrypt_version")
+
+    // Logging
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-config-yaml")
-    testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    // Configuration
+    implementation("com.sksamuel.hoplite:hoplite-core:$hoplite_version")
+    implementation("com.sksamuel.hoplite:hoplite-yaml:$hoplite_version")
 }
--- src/main/kotlin/HelloService.kt
+++
@@ -1,5 +0,0 @@
-package com.quadro
-
-fun interface HelloService {
-    fun sayHello()
-}

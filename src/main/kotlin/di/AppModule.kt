b6aa424957aa42b845c2342abeebc4fa7182cb2b
Kirill Matveev <playnikovv@vk.com>
2026-03-07 16:41:30 +0300

    update

--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -1,8 +1,20 @@
 package com.quadro.di
 
 import com.quadro.datasource.database.DatabaseFactory
-import com.quadro.datasource.repositories.*
-import com.quadro.domain.services.*
+import com.quadro.datasource.repositories.company.CompanyInvitationRepository
+import com.quadro.datasource.repositories.company.CompanyInvitationRepositoryImpl
+import com.quadro.datasource.repositories.company.CompanyMemberRepository
+import com.quadro.datasource.repositories.company.CompanyMemberRepositoryImpl
+import com.quadro.datasource.repositories.company.CompanyRepository
+import com.quadro.datasource.repositories.company.CompanyRepositoryImpl
+import com.quadro.datasource.repositories.users.UserRepository
+import com.quadro.datasource.repositories.users.UserRepositoryImpl
+import com.quadro.domain.services.auth.AuthService
+import com.quadro.domain.services.auth.AuthServiceImpl
+import com.quadro.domain.services.company.CompanyInvitationService
+import com.quadro.domain.services.company.CompanyInvitationServiceImpl
+import com.quadro.domain.services.company.CompanyService
+import com.quadro.domain.services.company.CompanyServiceImpl
 import com.quadro.presentation.auth.AuthController
 import com.quadro.presentation.company.CompanyController
 import com.quadro.security.*

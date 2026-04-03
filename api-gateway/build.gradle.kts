7f70323944fa24aa8fcac2022e021bbc8dd7377e
Kirill Matveev <playnikovv@vk.com>
2026-04-03 20:18:34 +0300

    fix jvmToolchain

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -54,3 +54,7 @@ dependencies {
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.mockk)
 }
+
+kotlin {
+    jvmToolchain(21)
+}
---
+++ api-gateway/src/test/kotlin/AuthServiceTest.kt
@@ -0,0 +1,16 @@
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class AuthServiceTest {
+
+    @Before
+    fun setup() {
+
+    }
+
+    @Test
+    fun `register - should create user successfully`() = runBlocking {
+
+    }
+}

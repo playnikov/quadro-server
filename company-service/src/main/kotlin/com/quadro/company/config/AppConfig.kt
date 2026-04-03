85dc576bac12d637dbda0e2e2c727fc8d869e0c0
Kirill Matveev <playnikovv@vk.com>
2026-04-03 22:55:30 +0300

    add Dockerfile + update config

--- company-service/src/main/kotlin/com/quadro/company/config/AppConfig.kt
+++ company-service/src/main/kotlin/com/quadro/company/config/AppConfig.kt
@@ -20,7 +20,8 @@ data class AppConfig(
                 database = DatabaseConfig(
                     host = System.getenv("DB_HOST") ?: "localhost",
                     port = (System.getenv("DB_PORT") ?: "5432").toInt(),
-                    name = System.getenv("DB_NAME") ?: "company",
+                    name = System.getenv("DB_NAME") ?: "company_db",
+                    schema = System.getenv("DB_SCHEMA") ?: "company_schema",
                     user = System.getenv("DB_USER") ?: "postgres",
                     password = System.getenv("DB_PASSWORD") ?: "postgres",
                     poolSize = (System.getenv("DB_POOL_SIZE") ?: "10").toInt()

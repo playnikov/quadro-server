1110e49cf914bf3cd70ecb3f0e3cc1842b949b70
Kirill Matveev <playnikovv@vk.com>
2026-04-03 22:55:37 +0300

    update

--- auth-service/src/main/kotlin/com/quadro/auth/config/AppConfig.kt
+++ auth-service/src/main/kotlin/com/quadro/auth/config/AppConfig.kt
@@ -20,7 +20,7 @@ data class AppConfig(
                 database = DatabaseConfig(
                     host = System.getenv("DB_HOST") ?: "localhost",
                     port = (System.getenv("DB_PORT") ?: "5432").toInt(),
-                    name = System.getenv("DB_NAME") ?: "auth",
+                    name = System.getenv("DB_NAME") ?: "auth_db",
                     schema = System.getenv("DB_SCHEMA") ?: "auth_schema",
                     user = System.getenv("DB_USER") ?: "postgres",
                     password = System.getenv("DB_PASSWORD") ?: "postgres",

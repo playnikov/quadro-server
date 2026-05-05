40ebaaa48f1cbcb6328f4f89bc9bb6c446180581
Kirill Matveev <playnikovv@vk.com>
2026-05-05 12:33:04 +0300

    remove

--- company-service/src/main/kotlin/com/quadro/company/domain/services/InvitationTokenService.kt
+++
@@ -1,9 +0,0 @@
-package com.quadro.company.domain.services
-
-import com.quadro.company.domain.models.InvitationValidationResult
-import java.util.UUID
-
-interface InvitationTokenService {
-    fun generateToken(invitationId: UUID, companyId: UUID, expiresInDays: Int? = 7): String
-    fun validateToken(token: String): InvitationValidationResult
-}
--- company-service/src/main/resources/db/migration/V1__create_copy.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users_copy (
-    id UUID PRIMARY KEY,
-    is_active BOOLEAN NOT NULL DEFAULT true
-);
-
-CREATE INDEX idx_users_copy_active ON users_copy(is_active);
